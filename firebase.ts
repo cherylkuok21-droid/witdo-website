1712b790d93ac2ffdea06da1eedce058645bc11f
cherylkuok21-droid <cherylkuok21@gmail.com>
2026-03-12 17:51:26 +0800

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,10 +1,35 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from 'firebase/auth';
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  query, 
+  orderBy, 
+  onSnapshot, 
+  Timestamp, 
+  doc, 
+  deleteDoc 
+} from 'firebase/firestore';
 import firebaseConfig from './firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
+
+export { 
+  signInWithPopup, 
+  GoogleAuthProvider, 
+  onAuthStateChanged, 
+  signOut,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  Timestamp,
+  doc,
+  deleteDoc
+};
 
 export default app;
