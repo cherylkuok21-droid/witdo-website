58ae66c8643c87d24888e3399d5452b62661d23e
cherylkuok21-droid <cherylkuok21@gmail.com>
2026-04-22 16:38:32 +0800

    Create types.ts

---
+++ src/components/types.ts
@@ -0,0 +1,4 @@
+
+export type Language = 'en' | 'zh';
+export type Page = 'home' | 'why' | 'about' | 'studio' | 'care' | 'designs' | 'faq' | 'giftcards';
+export type Category = 'duo' | 'full' | 'legacy';
