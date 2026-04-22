93ff06bd3ec310da1826d6d78d786a2a0538293d
cherylkuok21-droid <cherylkuok21@gmail.com>
2026-04-22 17:07:09 +0800

    Delete src/components/types.ts

--- src/components/types.ts
+++
@@ -1,4 +0,0 @@
-
-export type Language = 'en' | 'zh';
-export type Page = 'home' | 'why' | 'about' | 'studio' | 'care' | 'designs' | 'faq' | 'giftcards';
-export type Category = 'duo' | 'full' | 'legacy';
