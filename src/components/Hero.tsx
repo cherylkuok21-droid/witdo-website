f36fe82f9d8928a87602e790abc344a7f0285a86
cherylkuok21-droid <cherylkuok21@gmail.com>
2026-04-22 16:38:54 +0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { Language } from '@/App';
+import { Language } from '@/types';
 
 interface HeroProps {
   lang: Language;
