2ff06fc90b966ec41dca5071f38811fcbf0c1a8b
cherylkuok21-droid <cherylkuok21@gmail.com>
2026-04-22 16:40:43 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { Language } from '@/App';
+import { Language } from '@/types';
 
 interface FooterProps {
   lang: Language;
