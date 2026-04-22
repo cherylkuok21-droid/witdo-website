21455af1033734bd7c612c21144bb405878309cf
cherylkuok21-droid <cherylkuok21@gmail.com>
2026-04-22 16:52:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,6 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.NODE_ENV': JSON.stringify(mode),
-        'process': { env: {} },
       },
       optimizeDeps: {
         include: ['react-signature-canvas'],
